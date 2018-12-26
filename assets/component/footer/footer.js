1f3449cc6ab0ea1a8dd8aea41667dafee4c031de
EDZ <edz@EDZdeMacBook-Pro-3.local>
2018-12-26 17:18:52 +0800

    test

--- assets/component/footer/footer.js
+++ assets/component/footer/footer.js
@@ -14,25 +14,25 @@ Vue.component('component-footer',{
     template:`
         <footer class="footer">
             <div>
-                <img src="assets/images/icon/shyeselect.png" />
+                <img src="../assets/images/icon/shyeselect.png" />
                 <span class="footer_select">
                 首页
                 </span>
             </div>
             <div>
-                <img src="assets/images/icon/dashiunselect.png" />
+                <img src="../assets/images/icon/dashiunselect.png" />
                 <span>
                 大师讲堂
                 </span>
             </div>
             <div>
-                <img src="assets/images/icon/qifuunselect.png" />
+                <img src="../assets/images/icon/qifuunselect.png" />
                 <span>
                 祈福许愿
                 </span>
             </div>
             <div>
-                <img src="assets/images/icon/gerenunselect.png" />
+                <img src="../assets/images/icon/gerenunselect.png" />
                 <span>
                 个人中心
                 </span>
