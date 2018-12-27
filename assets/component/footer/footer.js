fb5f2689f99f0547274d4677eb08fea1b1560670
EDZ <edz@EDZdeMacBook-Pro-3.local>
2018-12-27 10:38:59 +0800

    change

--- assets/component/footer/footer.js
+++ assets/component/footer/footer.js
@@ -7,20 +7,22 @@ Vue.component('component-footer',{
     data(){
         return{
             footerList:[
-                {title:'首页',img:'../assets/images/icon/shyeunselect.png',active:false},
-                {title:'大师讲堂',img:'../assets/images/icon/shyeunselect.png',active:false},
-                {title:'祈福许愿',img:'../assets/images/icon/shyeunselect.png',active:false},
-                {title:'个人中心',img:'../assets/images/icon/shyeunselect.png',active:false}
+                {title:'首页',img:'../assets/images/icon/shyeunselect.png',active:false,pageUrl:'../footer1/index.html'},
+                {title:'大师讲堂',img:'../assets/images/icon/dashiunselect.png',active:false,pageUrl:'../footer2/home.html'},
+                {title:'祈福许愿',img:'../assets/images/icon/qifuunselect.png',active:false},
+                {title:'个人中心',img:'../assets/images/icon/gerenunselect.png',active:false}
             ]
         }
     },
     computed: {},
     methods:{
-
+        changePage(pageUrl){
+            window.location.href=pageUrl;
+        }
     },
     template:`
         <footer class="footer">
-            <div v-for="items in footerList">
+            <div v-for="items in footerList" @click='changePage(items.pageUrl)'>
                 <img :src="items.img" />
                 <span :class="items.active?'footer_select':''">{{items.title}}</span>
             </div>
