665ffdb94d65695c19df14dedf12a4451ab89b6a
pro <2665007122@qq.com>
2019-01-07 11:41:11 +0800

    foot3

--- assets/component/footer/footer.js
+++ assets/component/footer/footer.js
@@ -9,7 +9,7 @@ Vue.component('component-footer',{
             footerList:[
                 {title:'首页',img:'shyeunselect.png',active:false,pageUrl:'../footer1/index.html'},
                 {title:'大师讲堂',img:'dashiunselect.png',active:false,pageUrl:'../footer2/master.html'},
-                {title:'祈福许愿',img:'qifuunselect.png',active:false},
+                {title:'祈福许愿',img:'qifuunselect.png',active:false,pageUrl:'../footer3/index.html'},
                 {title:'个人中心',img:'gerenunselect.png',active:false,pageUrl:'../footer4/personalCenter.html'}
             ]
         }   
