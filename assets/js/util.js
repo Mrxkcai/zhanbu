e3d5d5aa32956ca2224a3dbfdf82479f20a59efd
EDZ <edz@EDZdeMacBook-Pro-3.local>
2019-01-11 16:42:37 +0800

    test

--- assets/js/util.js
+++ assets/js/util.js
@@ -15,32 +15,54 @@ function isUserInfo(key){
     }
 };
 
-//ajax
-var Ajax={
-    get: function(url, fn) {
-      // XMLHttpRequest对象用于在后台与服务器交换数据   
-      var xhr = new XMLHttpRequest();            
-      xhr.open('GET', url, true);
-      xhr.onreadystatechange = function() {
-        // readyState == 4说明请求已完成
-        if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) { 
-          // 从服务器获得数据 
-          fn.call(this, xhr.responseText);  
-        }
-      };
-      xhr.send();
-    },
-    // datat应为'a=a1&b=b1'这种字符串格式，在jq里如果data为对象会自动将对象转成这种字符串格式
-    post: function (url, data, fn) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", url, true);
-      // 添加http头，发送信息至服务器时内容编码类型
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");  
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304)) {
-          fn.call(this, xhr.responseText);
-        }
-      };
-      xhr.send(data);
-    }
+
+
+  var baseUrl = 'https://wxcs.nuoweibd.com/augury/';
+  function getData(url,params,type){
+    
+      // if(type == 'get'){
+      //     $.ajax({
+      //         url: baseUrl + url,
+      //         type:type,
+      //         data:params,
+      //         success:function(res){
+      //           // console.log(res)
+      //         }
+      //     })
+
+      // }
   }
+
+
+// 一位数组转二维数组
+function arr(n,array){
+    var num = n;//每个子数组里的元素个数
+    var arr = array;
+    var Arr = new Array(Math.ceil(arr.length/num));
+    for(var i = 0; i<Arr.length;i++){
+    Arr[i] = new Array();
+    for(var j = 0; j<num; j++){
+    Arr[i][j] = '';
+    }
+    }
+    for(var i = 0; i<arr.length;i++){
+        Arr[parseInt(i/num)][i%num] = arr[i]; 
+    } 
+
+    return Arr
+};
+
+
+//解析url
+function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+	var r = window.location.search.substr(1).match(reg); //匹配目标参数
+	if (r != null) return decodeURI(r[2]);
+	return null; //返回参数值
+};
+
+
+//转化个位数
+function tranformTow(number) {
+  return number < 10 ? '0'+number:number
+}
