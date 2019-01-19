8c25fc7320da415cee2a3da3b63037b6e40e3664
EDZ <edz@EDZdeMacBook-Pro-3.local>
2019-01-19 15:15:31 +0800

    test

--- assets/js/util.js
+++ assets/js/util.js
@@ -17,8 +17,16 @@ function isUserInfo(key){
 
 
 
-  var baseUrl = 'https://wxcs.nuoweibd.com/augury/';
+  var baseUrl = 'https://wxcs.nuoweibd.com/';
   var baseImgUrl = 'https://wxcs.nuoweibd.com/statics';
+  var Token = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyIiwidXNlcklkIjoiMSIsIm5hbWUiOiJoYWhhIiwiZXhwIjoxNTQ3ODk1NDUyfQ.hl57lRhFeMKTzlLQhWiawQqyEZQk80kEyJx1xFzQ_aLzSVEsKpfNP17YKJ7RcyZz-9P8SFJn3Z5KbmbsDr1ODtBwDAbDsi0-ZrJsGc4atRBC18DRGPH4OdVGbfksKua08_8oA1IYSQN-UuGVqo1fn6LUJlr8AtIobQ8EOTkFpmo';
+
+  // if(Token){
+  //     localStorage.setItem('token',Token);
+  // }else{
+    
+  // }
+
   function getData(url,params,type){
     
       // if(type == 'get'){
