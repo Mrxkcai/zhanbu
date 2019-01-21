6376b346f2ff7878ea45b3c80ef137a326fee98f
EDZ <edz@EDZdeMacBook-Pro-3.local>
2019-01-21 09:26:47 +0800

    test

--- assets/js/util.js
+++ assets/js/util.js
@@ -18,15 +18,48 @@ function isUserInfo(key){
 
 
 var baseUrl = 'https://wxcs.nuoweibd.com/';
-//var baseUrl = 'http://114.115.161.114:8220/';
+var baseUrl2 = 'http://114.115.161.114:8220/';
   var baseImgUrl = 'https://wxcs.nuoweibd.com/statics';
-  var Token = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyIiwidXNlcklkIjoiMSIsIm5hbWUiOiJoYWhhIiwiZXhwIjoxNTQ3ODk1NDUyfQ.hl57lRhFeMKTzlLQhWiawQqyEZQk80kEyJx1xFzQ_aLzSVEsKpfNP17YKJ7RcyZz-9P8SFJn3Z5KbmbsDr1ODtBwDAbDsi0-ZrJsGc4atRBC18DRGPH4OdVGbfksKua08_8oA1IYSQN-UuGVqo1fn6LUJlr8AtIobQ8EOTkFpmo';
-
-  // if(Token){
-  //     localStorage.setItem('token',Token);
-  // }else{
-    
-  // }
+  var Token = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyIiwidXNlcklkIjoiMSIsIm5hbWUiOiJoYWhhIiwiZXhwIjoxNTQ4MDQ2NDkwfQ.eQJpxXCQyn-PgC3uD_tOdPY7TPe_rSgfPVm1ja1L031KQl2_wODtoiy5zTDLgSvQZPn45AhqdpOrYq4irtiSIsNb4Lu_0xQX9fL6jzi54xxf1bexZ-eGWw3dt-_BMNGJhP6G_D5_conl7CKSym8Tgs5SPz5HW6bVnczDxQkvkQU';
+	
+  var Ajax={
+	  get: function(url, fn) {
+	    // XMLHttpRequest对象用于在后台与服务器交换数据   
+	    var xhr = new XMLHttpRequest();            
+	    xhr.open('GET', url, true);
+	    xhr.onreadystatechange = function() {
+	      // readyState == 4说明请求已完成
+	      if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) { 
+	        // 从服务器获得数据 
+	        fn.call(this, xhr.responseText);  
+	      }
+	    };
+	    xhr.send();
+	  },
+	  // datat应为'a=a1&b=b1'这种字符串格式，在jq里如果data为对象会自动将对象转成这种字符串格式
+	  post: function (url, data, fn) {
+	    var xhr = new XMLHttpRequest();
+	    xhr.open("POST", url, true);
+	    // 添加http头，发送信息至服务器时内容编码类型
+	    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");  
+	    xhr.setRequestHeader("Authorization", Token);  
+	    xhr.onreadystatechange = function() {
+	      if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304)) {
+	        fn.call(this, xhr.responseText);
+	      }
+	    };
+	    xhr.send(data);
+	  }
+}
+  var code = ''
+  Ajax.post(baseUrl + 'jwt/token', code, function(res){
+  		console.log(res)
+  })
+// if(Token){
+//   localStorage.setItem('token',Token);
+// }else{
+//
+// }
 
   function getData(url,params,type){
     
